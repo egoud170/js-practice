77041be1f553a89179d93659ca8a90899c4a8229
vijay <120800185+egoud170@users.noreply.github.com>
2024-01-11 17:26:41 +0000

    02_dataTypes.js created

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,23 @@
+const accontId=12345
+let accountEmail="viajy @gmail.com"
+var accountPassword="12545"
+accountCity="kurnool"
+
+// accontId=2313214  //const value cant be changed
+accountEmail='ajsdna'
+accountPassword='12313132'
+accountCity="goa"
+
+
+/*
+prefer Not to use var
+beacuse of issue in block scope and functional scope
+
+*/
+
+
+
+console.log(accontId);
+
+console.table([accontId,accountEmail,accountPassword,accountCity]);
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat javascript code as newer version
+let age=24
+let isLogedIn=false
+let stage = null
+
+
+// alert('hi') //here we are using node.js not browser
+
+//number=>2 to power 53
+//bignit
+//string=>""
+//boolean=> true?false
+//null=> standAlone value or empty value // it is object type
+//undefined
+
+//object
+//Array
+
+console.log(typeof(null));

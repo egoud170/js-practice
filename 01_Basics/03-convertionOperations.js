e0480a006c132ee3e17ca735fd47563a01a59a87
vijay <120800185+egoud170@users.noreply.github.com>
2024-01-11 17:53:11 +0000

    03_convertionoperation.js created

---
+++ 01_Basics/03-convertionOperations.js
@@ -0,0 +1,30 @@
+let age='24abc'
+
+console.log(typeof age)
+console.log(typeof (age))
+
+const number=Number(age)
+console.log(typeof (number))
+
+//'33'=>33
+//"33abc"=>NaN
+//true=>1 and false=>0
+
+ let isLogedIn=1
+  
+ let booleanlogedin=Boolean(isLogedIn)
+
+ console.log(booleanlogedin)
+
+ //1=>true
+
+ //0=>false
+
+ //'vijay'=.true
+
+ let someNumber=333
+
+ let stringNumber=String(someNumber)
+
+ console.log(stringNumber);//it is converted into string
+ console.log(typeof stringNumber);

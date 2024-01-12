400d5359ec319e96b5c26f4c7f8896ab4b9b751b
vijay <120800185+egoud170@users.noreply.github.com>
2024-01-12 18:18:39 +0000

    strings file created

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = 'vijay'
+const repoCount=6
+
+// console.log(name+repoCount+'value');
+
+console.log(`hi my name is ${name} and my repo is ${repoCount}`)
+
+
+const gameName= new String ('vijay')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('y'));
+
+const newString = gameName.substring(0,5)
+console.log(newString)
+
+const anotherString = gameName.slice(0,4)
+console.log(anotherString);
+
+const gray = '  jai  '
+console.log(gray);
+console.log(gray.trim());
+
+
+const url= "https//viajy.com/viajad%20sfnas"
+console.log(url)
+console.log(url.replace("%20","-"))
+

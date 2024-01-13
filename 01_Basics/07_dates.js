12837bf51647ac991539134c21af6439332c3a5e
vijay <120800185+egoud170@users.noreply.github.com>
2024-01-13 08:20:36 +0000

    07_Date.js file created

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,23 @@
+//dates
+
+let myDate= new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCretedDate = new Date(2023,0,23)
+// let myCretedDate = new Date(2023,0,23,5,3)
+// let myCretedDate = new Date("2023-01-20")
+// let myCretedDate = new Date("01-20-2022")
+// console.log(myCretedDate.toDateString());
+
+// let myTimeStamp= Date.now()
+
+// console.log(myTimeStamp);
+
+const mDate = new Date()
+console.log(mDate.getDate());
+console.log(mDate.getTime());
+console.log(mDate.getMonth()+1);
+console.log(mDate.getMinutes());
